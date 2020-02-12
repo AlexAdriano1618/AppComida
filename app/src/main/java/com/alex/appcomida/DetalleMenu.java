4c81d00407f1cd3b0516c874db9592b5560f4166
AlexAdriano1618 <alexliga1998@outlook.com>
2020-02-12 00:46:27 -0500

    Mapa

---
+++ app/src/main/java/com/alex/appcomida/DetalleMenu.java
@@ -0,0 +1,76 @@
+package com.alex.appcomida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.alex.appcomida.Modelo.clsMenu;
+import com.alex.appcomida.Rest.ConsumoRest;
+
+public class DetalleMenu extends AppCompatActivity {
+
+    ImageView img;
+    TextView txtNombrePlato, txtDetallePlato, txtCantidad1;
+    Button btnAumentar, btnDisminuir;
+    private int contador=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_menu);
+
+        txtNombrePlato = findViewById(R.id.txtNombrePlato);
+        txtDetallePlato = findViewById(R.id.txtDescripcionPlato);
+        txtCantidad1= findViewById(R.id.txtCantidad1);
+        btnAumentar = findViewById(R.id.btnAumentar);
+        btnDisminuir = findViewById(R.id.btnDisminuir);
+        img = findViewById(R.id.imageView);
+        Bundle bundle = getIntent().getExtras();
+
+
+        btnAumentar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador++;
+                txtCantidad1.setText(""+contador);
+            }
+        });
+
+        btnDisminuir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                contador --;
+                txtCantidad1.setText(""+contador );
+            }
+        });
+
+
+        int resid = bundle.getInt("resID");
+        img.setImageResource(resid);
+
+      /*  String resIDT4 = getIntent().getStringExtra("resIDT4");
+        String resIDT3 = getIntent().getStringExtra("resIDT3");
+        String resIDT2 = getIntent().getStringExtra("resIDT2");*/
+
+        String resIDT = getIntent().getStringExtra("resIDT");
+        txtNombrePlato.setText("-"+resIDT);
+
+
+
+       /* String dato = getIntent().getStringExtra("dato");
+        txtNombrePlato.setText("--"+ dato);*/
+        clsMenu menu = new clsMenu();
+        ConsumoRest consumo = new ConsumoRest();
+        menu=consumo.getDataMenuDetalle(resIDT);
+        txtNombrePlato.setText(menu.getPlato());
+        txtNombrePlato.setText(menu.getDescripcion());
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/alex/appcomida/Menu.java
+++ app/src/main/java/com/alex/appcomida/Menu.java
@@ -2,13 +2,150 @@ package com.alex.appcomida;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.alex.appcomida.Modelo.clsMenu;
 
-public class Menu extends AppCompatActivity {
+import com.alex.appcomida.Rest.ConsumoRest;
 
+import java.util.ArrayList;
+
+public class Menu extends AppCompatActivity  implements View.OnClickListener{
+
+    TextView txt1, txt2, txt3, txt4, pre1, pre2, pre3, pre4;
+    String cod1, cod2, cod3, cod4;
+    ImageView img1,img2,img3,img4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        txt1 = findViewById(R.id.txtProd1);
+        txt2 = findViewById(R.id.txtprod2);
+        txt3 = findViewById(R.id.txtProd3);
+        txt4 = findViewById(R.id.txtProd4);
+        pre1 = findViewById(R.id.txtPrecio1);
+        pre2 = findViewById(R.id.txtprecio2);
+        pre3 = findViewById(R.id.txtprecio3);
+        pre4 = findViewById(R.id.txtPrecio4);
+        getData();
+
+    }
+
+    public void EnviarDatosPlato1 (View view){
+        Intent abrir_prod1 = new Intent(Menu.this, DetalleMenu.class);
+        abrir_prod1.putExtra("resID",R.drawable.seco_guanta);
+        abrir_prod1.putExtra("resIDT", cod1.toString());
+
+
+
+        startActivity(abrir_prod1);
+    }
+
+    public void EnviarDatosPlato2 (View view){
+        Intent abrir_prod2 = new Intent(Menu.this, DetalleMenu.class);
+        abrir_prod2.putExtra("resID",R.drawable.seco_pollo);
+        abrir_prod2.putExtra("resIDT2", cod2.toString());
+
+        startActivity(abrir_prod2);
+    }
+
+    public void EnviarDatosPlato3 (View view){
+        Intent abrir_prod3 = new Intent(Menu.this, DetalleMenu.class);
+        abrir_prod3.putExtra("resID",R.drawable.fritada);
+        abrir_prod3.putExtra("resIDT3", cod3.toString());
+
+        startActivity(abrir_prod3);
+    }
+
+    public void EnviarDatosPlato4 (View view){
+        Intent abrir_prod4 = new Intent(Menu.this, DetalleMenu.class);
+        abrir_prod4.putExtra("resID",R.drawable.ceviche_camaron);
+        abrir_prod4.putExtra("resIDT4", cod4.toString());
+
+        startActivity(abrir_prod4);
+    }
+
+
+    public void getData() {
+
+        ConsumoRest datos = new ConsumoRest();
+        ArrayList<clsMenu> Datas = datos.getDataMenu();
+        for (int i = 0; i < Datas.size(); i++) {
+            if (i == 0 )  {
+                cod1 = Integer.toString(Datas.get(i).getCodigo());
+                txt1.setText(Datas.get(i).getPlato());
+                pre1.setText(Double.toString(Datas.get(i).getPrecio()));
+            } else if (i == 1) {
+                cod2 = Integer.toString(Datas.get(i).getCodigo());
+                txt2.setText(Datas.get(i).getPlato());
+                pre2.setText(Double.toString(Datas.get(i).getPrecio()));
+            } else if (i == 2) {
+                cod3 = Integer.toString(Datas.get(i).getCodigo());
+                txt3.setText(Datas.get(i).getPlato());
+                pre3.setText(Double.toString(Datas.get(i).getPrecio()));
+            } else if (i == 3) {
+                cod4 = Integer.toString(Datas.get(i).getCodigo());
+                txt4.setText(Datas.get(i).getPlato());
+                pre4.setText(Double.toString(Datas.get(i).getPrecio()));
+            }
+
+
+        }
+
+    }
+
+   /* public void plato1(){
+        Intent abrir_prod1 = new Intent(Menu.this, DetalleMenu.class);
+        abrir_prod1.putExtra("resID",R.drawable.seco_guanta);
+        startActivity(abrir_prod1);
+    }*/
+
+
+
+    /*Intent i = new Intent(this, DetalleMenu.class);
+        i.putExtra("dato", txt1.getText().toString());
+    startActivity(i);*/
+
+
+
+    /*  public void plato2(){
+          Intent abrir_prod1 = new Intent(Menu.this, DetalleMenu.class);
+          abrir_prod1.putExtra("resID",R.drawable.seco_pollo);
+          abrir_prod1.putExtra("resIDT", txt1.getText().toString());
+          startActivity(abrir_prod1);
+
+      }*/
+    public void plato3(){
+        Intent abrir_prod1 = new Intent(Menu.this, DetalleMenu.class);
+        abrir_prod1.putExtra("resID",R.drawable.fritada);
+        startActivity(abrir_prod1);
+
+    }
+    public void plato4(){
+        Intent abrir_prod1 = new Intent(Menu.this, DetalleMenu.class);
+        abrir_prod1.putExtra("resID",R.drawable.ceviche_camaron);
+        startActivity(abrir_prod1);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cv0:
+                //  plato1();
+                break;
+            case R.id.cv1:
+                //   plato2();
+                break;
+            case R.id.cv2:
+                //    plato3();
+                break;
+            case R.id.cv3:
+                // plato4();
+                break;
+        }
     }
 }
