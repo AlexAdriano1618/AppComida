2be55fc83e3ba4663540a7b1c63b5ad5fc15c704
AlexAdriano1618 <alexliga1998@outlook.com>
2020-02-11 23:48:41 -0500

    Login

--- app/src/main/java/com/alex/appcomida/MainActivity.java
+++ app/src/main/java/com/alex/appcomida/MainActivity.java
@@ -2,13 +2,57 @@ package com.alex.appcomida;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.alex.appcomida.Rest.ConsumoRest;
 
 public class MainActivity extends AppCompatActivity {
+
+    //defining view objects
+    EditText TextUsuario, TextPassword;
+    Button btnRegistrar, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextUsuario = findViewById(R.id.txt_usuario);
+        TextPassword = findViewById(R.id.txt_password);
+        btnRegistrar = findViewById(R.id.btn_registrar);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener((View.OnClickListener) this);
+        btnRegistrar.setOnClickListener((View.OnClickListener) this);
+    }
+    private void registrarUsuario() {
+
+        Intent abrir_reg = new Intent(MainActivity.this, Registro.class );
+        startActivity(abrir_reg);
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_registrar:
+                registrarUsuario();
+                break;
+            case R.id.btnLogin:
+                ConsumoRest consumo = new ConsumoRest();
+                if(consumo.getDataUsuarios(TextUsuario.getText().toString(), TextPassword.getText().toString())){
+                    Intent abrir_reg = new Intent(MainActivity.this, Menu.class );
+                    startActivity(abrir_reg);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Credenciales incorrectas", Toast.LENGTH_SHORT);
+                }
+
+
+                // loginUsuario1();
+                break;
+        }
     }
 }
